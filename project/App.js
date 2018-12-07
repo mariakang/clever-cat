4cf6024740a0304f5e19071cd3a0592f6973e704
mariakang <45389455+mariakang@users.noreply.github.com>
2018-12-07 09:39:18 +0000

    Update App.js

--- project/App.js
+++ project/App.js
@@ -1,5 +1,5 @@
-import React from 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js';
-import ReactDOM from 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.1/react-dom.min.js';
+//import React from 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js';
+//import ReactDOM from 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.1/react-dom.min.js';
 import App from './components/App';
 
 ReactDOM.render(<App name="Foo" />, document.getElementById('app'));
