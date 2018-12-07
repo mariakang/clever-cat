9463fcdea4479408cc4fdf9dc52f5ab1d6e1422e
mariakang <45389455+mariakang@users.noreply.github.com>
2018-12-07 09:55:12 +0000

    Update and rename project/components/App.js to mortgage-calculator/components/App.js

--- mortgage-calculator/components/App.js
+++ mortgage-calculator/components/App.js
@@ -1,9 +1,3 @@
-//import React from 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js';
-//import Func from './../public/functions.js';
-//import BarChart from './BarChart.js';
-//import Field from './Field.js';
-//import Overpayment from './Overpayment.js';
-
 class App extends React.Component{
   constructor(props) {
     super(props);
