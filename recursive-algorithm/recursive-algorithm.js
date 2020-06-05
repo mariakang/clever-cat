a2b224e008981cfdb852d0b38415d676332c85ed
mariakang <45389455+mariakang@users.noreply.github.com>
2020-06-05 13:54:32 +0100

    Update recursive-algorithm.js

--- recursive-algorithm/recursive-algorithm.js
+++ recursive-algorithm/recursive-algorithm.js
@@ -258,12 +258,14 @@ function runTests() {
     let name = "p(n) average runtime";
     let color = "hsl(240, 50%, 50%)";
     let dataObject = createChartDataObject(name, color, dataPointsArr);
-    let compName = "1/10000000 * 2^n";
+    let divisorsArr = dataPointsArr.filter(obj => obj.x >= maxN - 5).map(obj => Math.pow(2, obj.x) / obj.y);
+    let divisor = divisorsArr.reduce((x, y) => x + y) / divisorsArr.length;
+    let compName = "1/" + divisor + " * 2^n";
     let compColor = color.replace("50%)", "85%)");
     let compDataPointsArr = testSizes.map(function (n) {
       return {
         x: n,
-        y: round(Math.pow(2, n) / 10000000)
+        y: round(Math.pow(2, n) / divisor)
       };
     });
     let compDataObject = createChartDataObject(compName, compColor, compDataPointsArr);
