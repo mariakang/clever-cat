25cf5bf043d9851a8c745b21b23621fe820645d3
mariakang <45389455+mariakang@users.noreply.github.com>
2021-08-12 13:36:04 +0100

    Update Home.js

--- memorise/js/components/Home.js
+++ memorise/js/components/Home.js
@@ -60,7 +60,7 @@ class Home extends React.Component{
                 username: username
               }
             }).then((response) => {
-                console.log(response);
+                console.log(JSON.stringify(response));
                 if (response.ok) {
                   this.setState({
                     mode: "home",
