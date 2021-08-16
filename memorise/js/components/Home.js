2cdeb569803c5258b6e5ec6a7e79066a77d05b59
mariakang <45389455+mariakang@users.noreply.github.com>
2021-08-16 13:13:23 +0100

    Update Home.js

--- memorise/js/components/Home.js
+++ memorise/js/components/Home.js
@@ -290,7 +290,7 @@ class Home extends React.Component{
       modal = (
         <div className="modalContainer">
           <div className="modal">
-            <div className="red">{this.state.errorMessage}<br/></div>
+            <div className="modalText red">{this.state.errorMessage}</div>
             <button onClick={this.handleAcknowledgeError}>OK</button>
           </div>
         </div>
